898ae57b5ddd3e69caca074a164ba14787c641c1
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-04-06 02:22:55 +0700

    cart

---
+++ src/screens/CartScreen.jsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { cartAddItem, cartRemoveItem } from "../store/slices/cartSlice";
+import { useNavigate } from "react-router-dom";
+import Massage from "../components/Massage";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+
+function CartScreen() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const removeFromCartHandler = (id) => {
+    dispatch(cartRemoveItem(id));
+  };
+  const checkoutHandler = () => {
+    console.log("1223");
+    navigate("/login?redirect=shipping");
+  };
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping cart</h1>
+        {cartItems.length === 0 ? (
+          <Massage variant="info">
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Massage>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={`http://127.0.0.1:8000/${item.image}`}
+                      alt={item.image}
+                      fluid
+                      rounded
+                    />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={3}>
+                    {item?.countInStock > 0 && (
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            cartAddItem({
+                              id: item.product,
+                              qty: Number(e.target.value), // Chuyển đổi giá trị từ chuỗi thành số
+                              pDetails: cartItems,
+                            })
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option value={x + 1} key={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    )}
+                  </Col>
+                  <Col md={1}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              <span>
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.qty * item.price, 0)
+                  .toFixed()}
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                disabled={cartItems.length === 0}
+                type="button"
+                className="btn-block w-100"
+                onClick={checkoutHandler}
+              >
+                PROCEED TO CHECKOUT
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
+export default CartScreen;
---
+++ src/store/slices/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateCartItems = {
+  cartItems: [],
+};
+
+export const cartItems = createSlice({
+  name: "cartItems",
+  initialState: initialStateCartItems,
+  reducers: {
+    cartAddItem: (state, action) => {
+      const { id, qty, pDetails } = action.payload;
+
+      const { _id, name, image, price, countInStock } = pDetails;
+
+      const existItemIndex = state.cartItems.findIndex(
+        (x) => x.product === Number(id)
+      );
+      if (existItemIndex !== -1) {
+        // Nếu mục đã tồn tại, thay thế số lượng của nó bằng qty mới
+        state.cartItems[existItemIndex].qty = parseInt(qty, 10);
+      } else {
+        // Nếu mục không tồn tại, thêm vào giỏ hàng
+        state.cartItems.push({
+          product: _id,
+          name,
+          image,
+          price,
+          countInStock,
+          qty: parseInt(qty, 10),
+        });
+      }
+    },
+    cartRemoveItem: (state, action) => {
+      const productIdToRemove = action.payload;
+      state.cartItems = state.cartItems.filter(
+        (item) => item.product !== productIdToRemove
+      );
+    },
+  },
+});
+
+export const { cartAddItem, cartRemoveItem } = cartItems.actions;
+export default cartItems.reducer;
