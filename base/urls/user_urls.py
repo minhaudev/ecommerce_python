1f0fa1b032d080f5bf1f7f9df23a43215aff2388
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-04-16 00:10:53 +0700

    create crud user

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('',views.getUsers, name='users'),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
     path('register/',views.registerUser, name='register'),
-   
+    path('<str:pk>/',views.getUserById, name='user'),
+    path('update/<str:pk>/',views.updateUser, name='user-update'),
+    path('delete/<str:pk>/',views.deleteUser, name='delete'),
 ]
--- base/views/user_views.py
+++ base/views/user_views.py
@@ -64,5 +64,31 @@ def updateUserProfile(request):
         user.password = make_password(data['password'])
     user.save()
     return Response(serializers.data)
-   
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser (request, pk):
+    userForDeletion = User.objects.get(id = pk)
+    userForDeletion.delete()
+    return Response("User was deleted")
      
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request,pk):
+    user = User.objects.get(id= pk)
+    serializers = UserSerializer(user, many= False)
+    return Response(serializers.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id = pk)
+    data = request.data 
+
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+    user.save()
+    serializers  = UserSerializer(user, many= False)
+    return Response(serializers.data)
